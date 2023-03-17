746d6566ea5fba7ea11ba812344f6a7779d7e7a7
pychangeric <pychangeric@gmail.com>
2023-03-17 15:08:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName="bob";
+const leastFavoriteCustomer="job";
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+function setBestCustomer(){
+    window.bestCustomer="BOB";
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="ola";
+}
+console.log(leastFavoriteCustomer);
+function setBestCustomer(){
+
+}
